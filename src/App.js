cecd551a208803c8f28152ecae9c7ff6dd7ae63f
Rajeevalochan_R <rajeevalochan.r@contus.in>
2019-06-29 12:31:34 +0530

    added validation feature

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import SearchProfile from './components/SearchProfile';
-import Profile from './components/Profile';
-import './App.css';
-function App() {
+import React, { useState, useEffect } from "react";
+import SearchProfile from "./components/SearchProfile";
+import Profile from "./components/Profile";
+import "./App.css";
+import githubUsernameRegex from "github-username-regex";
+import toastr from "toastr";
 
-  const API = 'https://api.github.com/users';
+function App() {
+  const API = "https://api.github.com/users";
 
   const [state, setState] = useState({
-    username: 'dinesh0666',
-    name: '',
-    avatar: '',
-    location: '',
-    repos: '',
-    followers: '',
-    following: '',
-    homeUrl: '',
-    notFound: ''
+    username: "dinesh0666",
+    name: "",
+    avatar: "",
+    location: "",
+    repos: "",
+    followers: "",
+    following: "",
+    homeUrl: "",
+    notFound: ""
   });
 
-  const fetchProfile = function (username){
-    let url = `${API}/${username}`;
-    fetch(url)
-      .then((res) => res.json() )
-      .then((data) => {
-        setState({
-          username: data.login,
-          name: data.name,
-          avatar: data.avatar_url,
-          location: data.location,
-          repos: data.public_repos,
-          followers: data.followers,
-          following: data.following,
-          homeUrl: data.html_url,
-          notFound: data.message,
-          bio:data.bio
+  const fetchProfile = function(username) {
+    if (githubUsernameRegex.test(username)) {
+      let url = `${API}/${username}`;
+      fetch(url)
+        .then(res => res.json())
+        .then(data => {
+          setState({
+            username: data.login,
+            name: data.name,
+            avatar: data.avatar_url,
+            location: data.location,
+            repos: data.public_repos,
+            followers: data.followers,
+            following: data.following,
+            homeUrl: data.html_url,
+            notFound: data.message,
+            bio: data.bio
+          });
         })
-      })
-      .catch((error) => console.log('Oops! . There Is A Problem') )
-  }
+        .catch(error => console.log("Oops! . There Is A Problem"));
+    } else {
+      toastr.error("Please Enter the Valid Github username");
+    }
+  };
 
   useEffect(() => {
     fetchProfile(state["username"]);
@@ -45,11 +51,10 @@ function App() {
   return (
     <div>
       <h1 className="heading">Search Git Hub Profile</h1>
-      <SearchProfile fetchProfile = {fetchProfile}/>
+      <SearchProfile fetchProfile={fetchProfile} />
       <Profile data={state} />
     </div>
-  )
+  );
 }
-
 
 export default App;
--- src/components/SearchProfile.jsx
+++ src/components/SearchProfile.jsx
@@ -1,24 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function SearchProfile(props){
-    
-    const [username,setUsername] = useState();
+function SearchProfile(props) {
+  const [username, setUsername] = useState();
 
-    const changeText = function(e){
-        setUsername(e.target.value);
-    }
-    const handleForm = function (e) {
-        e.preventDefault();
-        props.fetchProfile(username);
-         setUsername('')
-    }
-    return (
-        <div className="search--box" >
-        <form onSubmit={(e) => handleForm(e)}>
-          <label><input type="search" onChange={(e) => changeText(e)} name={"username"} value={username}  placeholder="Type Username + Enter"/></label>
-        </form>
-        </div>
-      )
-
+  const changeText = function(e) {
+    setUsername(e.target.value);
+  };
+  const handleForm = function(e) {
+    e.preventDefault();
+    props.fetchProfile(username);
+    setUsername("");
+  };
+  return (
+    <div className="search--box">
+      <form onSubmit={e => handleForm(e)}>
+        <label>
+          <input
+            type="search"
+            autoComplete="off"
+            onChange={e => changeText(e)}
+            name={"username"}
+            value={username}
+            placeholder="Type Username + Enter"
+          />
+        </label>
+      </form>
+    </div>
+  );
 }
 export default SearchProfile;
